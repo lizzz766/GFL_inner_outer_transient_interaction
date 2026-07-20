c06a675f7e27117da8ce9cb03de07b263a0708c2
Li Zhongze <72335230+lizzz766@users.noreply.github.com>
2026-07-20 12:50:05 +0800

    Add package initializer

---
+++ src/gfl_interaction/__init__.py
@@ -0,0 +1,5 @@
+"""GFL inner/outer transient interaction research code."""
+
+from .model import GFLParameters, SimulationResult, simulate_fourth_order
+
+__all__ = ["GFLParameters", "SimulationResult", "simulate_fourth_order"]
