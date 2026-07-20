3dc34bc3a40084ff8afe89230bdb39331aab0518
Li Zhongze <72335230+lizzz766@users.noreply.github.com>
2026-07-20 12:57:17 +0800

    Expose swing and PI energy analysis APIs

--- src/gfl_interaction/__init__.py
+++ src/gfl_interaction/__init__.py
@@ -1,5 +1,12 @@
-"""GFL inner/outer transient interaction research code."""
+"""GFL inner-outer transient interaction research code."""
 
 from .model import GFLParameters, SimulationResult, simulate_fourth_order
+from .energies import swing_energy_trace, pi_energy_trace
 
-__all__ = ["GFLParameters", "SimulationResult", "simulate_fourth_order"]
+__all__ = [
+    "GFLParameters",
+    "SimulationResult",
+    "simulate_fourth_order",
+    "swing_energy_trace",
+    "pi_energy_trace",
+]
